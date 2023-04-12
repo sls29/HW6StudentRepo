57e42a8b829356e4898662888824a824df1e1291
roger <seles.lucian@gmail.com>
2023-04-12 11:13:17 +0300

    ver1

---
+++ src/main/java/StudentRepo/App.java
@@ -0,0 +1,13 @@
+package StudentRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
